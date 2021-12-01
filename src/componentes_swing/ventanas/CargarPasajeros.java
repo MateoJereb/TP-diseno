9eceb6865cce3a2ac02f536e35b123183be152ab
MateoJereb <mateojereb@gmail.com>
2021-11-30 23:08:25 -0300

    interfaces OcuparHabitacion y CargarPasajeros

--- src/componentes_swing/ventanas/CargarPasajeros.java
+++ src/componentes_swing/ventanas/CargarPasajeros.java
@@ -136,7 +136,7 @@ public class CargarPasajeros extends JPanel{
 		cons.anchor = GridBagConstraints.NORTH;
 		panelSuperior.add(panelInformacion,cons);
 		
-		eInfo = new EtiquetaJ("<html><body>Se está ocupando la habitación:<br>&emsp\u2022Nro: "+habitacion.getNro()+"<br>&emsp\u2022Tipo: "+textoTipoHab(habitacion)+"<br><br></html></body>");
+		eInfo = new EtiquetaJ("<html><body>Se está ocupando la habitación:<br><br>&emsp\u2022Nro: "+habitacion.getNro()+"<br><br>&emsp\u2022Tipo: "+textoTipoHab(habitacion)+"<br><br></html></body>");
 		
 		cons.weighty = 0.1;
 		cons.weightx = 0.1;
